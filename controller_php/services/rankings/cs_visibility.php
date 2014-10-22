57ec02d1cfba319d8463410f50f5db13a600e76a
franziskahempel <f.hempel@searchmetrics.com>
2014-10-22 14:43:28 +0200

    fixed UX-1351 php layer

---
+++ controller_php/services/rankings/cs_visibility.php
@@ -0,0 +1,34 @@
+<?php
+//Name of Service Endpoint
+$service = 'RankingsCsVisibility';
+
+//Test Params as array()
+$params = array(
+	'user_id'		=> '4925bdf59b400b4a10f076d0f1f06fa8',
+	'project_id'	=> '268317',
+	'domains'		=> array('puls4.com'),
+	'se_id'			=> '26'
+);
+
+//Require ApiRequest Class and make API Request
+require_once('../../ApiRequest.php');
+$api_request = new ApiRequest();
+$response = $api_request->get($service,$params);
+
+//Transform Response Data as needed for KPI
+$transformed_response = array();
+if(isset($response['response'])){
+	if(isset($response['response'][0]['sum_visibility']))
+	{	
+		$transformed_response['value'] = $response['response'][0]['sum_visibility'];
+	}
+	if(isset($response['response'][0]['trend']['per']))
+	{	
+		$transformed_response['trend'] = round($response['response'][0]['trend']['per'],2);
+	}
+}
+
+//Output transformed Response
+echo json_encode($transformed_response);
+
+?>
